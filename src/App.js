c9e0b1a6d85db78b9e981b42e869a3a3d7cab9de
HuongLa <chipite77@gmail.com>
2020-11-11 16:53:34 +0700

    'header_component'

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
-import { Route, Switch } from 'react-router-dom'
-import TrangChu from './Page/TrangChu';
-import TinTuc from './Page/TinTuc';
-import LienHe from './Page/LienHe';
-import UngDung from './Page/UngDung';
-import DangNhap from './Page/DangNhap';
-import DangKy from './Page/DangKy';
+
 
 function App() {
   return (
     <>
       <Header />
 
-      <Switch>
-        <Route path='/trangchu' component={TrangChu} />
-        <Route path='/tintuc' component={TinTuc} />
-        <Route path='/lienhe' component={LienHe} />
-        <Route path='/ungdung' component={UngDung} />
-
-        {/* Router mặc định */}
-        <Route path='/' component={TrangChu} />
-      </Switch>
+     
 
       <Footer />
     </>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,64 +1,41 @@
-import React, { useState } from 'react'
-import { NavLink } from 'react-router-dom'
-import DangKy from '../Page/DangKy'
-import DangKyDangNhapModal from '../Page/DangKyDangNhapModal'
-import DangNhap from '../Page/DangNhap'
-
-// import './Header.module.scss'
-
-//HOC
-const DangKyModal = new DangKyDangNhapModal("Đăng ký", DangKy)
-const DangNhapModal = new DangKyDangNhapModal("Đăng nhập", DangNhap)
+import React from "react";
+import style from "../Css/main.css";
 
 export default function Header() {
-
-    const [state, setState] = useState('DK')
-    const renderModal = () => {
-        if (state === 'DK') {
-            return DangKyModal
-        }
-        else {
-            return DangNhapModal
-        }
-    }
-
-    return (
-        <nav className="navbar navbar-expand-sm navbar-light bg-light">
-            <div>
-                <a className="navbar-brand" href="#">Navbar</a>
-                <button className="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon" />
-                </button>
-            </div>
-            <div className="collapse navbar-collapse d-flex text-center justify-content-center" id="collapsibleNavId">
-                <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-                    <li className="nav-item active">
-                        <NavLink className="nav-link chuTrang" to='/trangchu'>Trang chủ<span className="sr-only">(current)</span></NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink className="nav-link" to='/lienhe'>Liên hệ</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink className="nav-link" to='/tintuc'>Tin tức</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink className="nav-link" to='/ungdung'>Ứng dụng</NavLink>
-                    </li>
-
-                </ul>
-            </div>
-            <div>
-
-                <button className="btn btn-outline-danger" data-toggle="modal" data-target="#modelId" onClick={() => {
-                    setState('DK')
-                }}>Đăng ký</button>
-                <button className="btn btn-outline-danger" data-toggle="modal" data-target="#modelId" onClick={() => {
-                    setState('DN')
-                }}>Đăng nhập</button>
-                {renderModal()}
-
-            </div>
-        </nav>
-
-    )
+  return (
+    <header>
+      <div className="row align-items-center">
+        <div className="col-3">
+          <img className="weblogo" src="./img/web-logo.png" />
+        </div>
+        <div className="col-6 header__menu">
+          <ul>
+            <li>
+              <a href="#">Lịch chiếu</a>
+            </li>
+            <li>
+              <a href="#">Cụm rạp</a>
+            </li>
+            <li>
+              <a href="#">Tin tức</a>
+            </li>
+            <li>
+              <a href="#">Ứng dụng</a>
+            </li>
+          </ul>
+        </div>
+        <div className="col-3 d-flex header__right">
+          <div className="account">
+            <img src="./img/avatar.png" />
+            <p>Đăng nhập</p>
+          </div>
+          <div>|</div>
+          <div className="location">
+            <img src="./img/location-header.png" />
+            <p>Hồ Chí Minh</p>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
