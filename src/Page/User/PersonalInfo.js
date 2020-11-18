39edcfc7f1978e44a7374e2483e7a0e15fe0a505
Trung Lư <lutrung>
2020-11-18 16:02:32 +0700

    'personalInfo'

--- src/Page/User/PersonalInfo.js
+++ src/Page/User/PersonalInfo.js
@@ -1,5 +1,4 @@
 import React from 'react'
-
 export default function PersonalInfo(props) {
     return (
         <div className='PersonalInfo_conent'>
@@ -29,6 +28,39 @@ export default function PersonalInfo(props) {
                         <span className="carousel-control-next-icon" aria-hidden="true" />
                         <span className="sr-only">Next</span>
                     </a>
+                </div>
+            </div>
+            <div className='personalInfo_info container'>
+                <div>
+                    <nav>
+                        <div className="nav nav-tabs" id="nav-tab" role="tablist">
+                            <a className="nav-link active" id="nav-home-tab" data-toggle="tab" href="#personal-info" role="tab" aria-controls="nav-home" aria-selected="true">THÔNG TIN CÁ NHÂN</a>
+                            <a className="nav-link" id="nav-profile-tab" data-toggle="tab" href="#history-booking" role="tab" aria-controls="nav-profile" aria-selected="false">LỊCH SỬ ĐẶT VÉ</a>
+                        </div>
+                    </nav>
+                    <div className="tab-content" id="nav-tabContent">
+                        <div className="tab-pane fade show active" id="personal-info" role="tabpanel" aria-labelledby="nav-home-tab">
+                            <div className='personal_information'>
+                                <div className='row'>
+                                    <div className='col-6 personalInfo_left'>
+                                        <p>Email: <span></span></p>
+                                        <p>Họ tên: <span></span></p>
+                                        <p>Số điện: <span></span></p>
+                                    </div>
+                                    <div className='col-6 personalInfo_right'>
+                                        <p>Tài khoản: <span></span></p>
+                                        <p>Mật khẩu: <span></span></p>
+                                        <a href=''>Cập nhật</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="tab-pane fade" id="history-booking" role="tabpanel" aria-labelledby="nav-profile-tab">
+                            <div className='history_booking'>
+                                LỊCH SỬ ĐẶT VÉ
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
             </div>
